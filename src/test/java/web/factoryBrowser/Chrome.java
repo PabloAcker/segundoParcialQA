20328c23509a8b7b2b02a3c28a53f364b6b18cf8
PabloAcker <favw14@gmail.com>
2023-10-18 02:05:36 -0400

    Commit with dependencies

--- src/test/java/web/factoryBrowser/Chrome.java
+++ src/test/java/web/factoryBrowser/Chrome.java
@@ -7,7 +7,7 @@ import java.time.Duration;
 public class Chrome implements IBrowser{
     @Override
     public WebDriver create() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chrome/chromedriver.exe");
         WebDriver chrome = new ChromeDriver();
         chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         chrome.manage().window().maximize();
